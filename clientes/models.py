a4f3554e48f9baf5518703d468932d53df1affb6
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-07-23 09:48:35 -0300

    COMMIT_VALIDATION

--- clientes/models.py
+++ clientes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField(blank=False, max_length=30, )
+    email = models.EmailField(blank=False, max_length=30)
     cpf = models.CharField(max_length=11, unique=True)
     rg = models.CharField(max_length=9)
     celular = models.CharField(max_length=14)
--- clientes/serializers.py
+++ clientes/serializers.py
@@ -5,4 +5,7 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+    def validade_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O cpf deve ter 11 dígitos")
+        return cpf
